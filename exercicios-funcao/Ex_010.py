34c2366f9d5162532b288b74bab3dfaed0cd7905
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-06 12:01:49 -0300

    Update Ex_010.py

--- exercicios-funcao/Ex_010.py
+++ exercicios-funcao/Ex_010.py
@@ -1,8 +1,17 @@
 class GerarVetor:
+    """
+    classe: gerar um vetor
+    """
     def __init__(self):
         self.vetor = list()
 
     def gerar_vetor(self, tamanho):
+
+        """
+        :param tamanho: int
+        :return: gera vetor com tamanho n
+        """
+
         from random import randint
 
         while len(self.vetor) != tamanho:
@@ -11,20 +20,39 @@ class GerarVetor:
                 self.vetor.append(e)
 
     def imprimir_vetor(self):
+        """
+        :return: o prórpio vetor
+        """
+
         return self.vetor
 
     def ordenar_vetor(self):
+        """
+        :return: vetor ordenado com a função sorted
+        """
+
         self.vetor = sorted(self.vetor)
         return self.vetor
 
     def buscar_elemento(self, elemento):
+        """
+        :param elemento: int
+        :return: posição do valor se houver
+        ou -1 caso não exista (bisca binária)
+        """
+
         vetor = self.ordenar_vetor()
 
+        # cariáveis para manipular a busca
         inicio = 0
         fim = len(vetor)//2 - 1
 
         while inicio <= fim:
+
+            # meio = indice
             meio = (inicio + fim) // 2
+
+            # chute = elemento
             chute = vetor[meio]
             if chute == elemento:
                 return meio
@@ -35,6 +63,10 @@ class GerarVetor:
         return -1
 
     def maior_elemento(self):
+        """
+        :return: maior elemento
+        """
+
         maior = self.vetor[0]
         for i in self.vetor[1::]:
             if maior < i:
@@ -42,6 +74,9 @@ class GerarVetor:
         return maior
 
     def menor_elemento(self):
+        """
+        :return: menor elemento
+        """
         menor = self.vetor[0]
         for i in self.vetor[1::]:
             if menor > i:
@@ -49,6 +84,9 @@ class GerarVetor:
         return menor
 
     def media_valor(self):
+        """
+        :return: media entre os elementos
+        """
         soma = self.vetor[0]
         for i in self.vetor[1::]:
             soma += i
