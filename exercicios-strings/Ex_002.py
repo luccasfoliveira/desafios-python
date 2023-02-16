84e74755fc559e436641d9ae13030aa52352839c
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-16 01:25:47 -0300

    update

--- exercicios-strings/Ex_002.py
+++ exercicios-strings/Ex_002.py
@@ -2,8 +2,9 @@ texto = input("Texto: ")
 verificadas = list()
 for vogal in texto:
     if vogal in ['A', 'E', 'I', 'O', 'U'] and vogal not in verificadas:
-        print(f'{vogal} = {texto.count(vogal)}')
         verificadas.append(vogal)
     elif vogal in ['a', 'e', 'i', 'o', 'u'] and vogal not in verificadas:
-        print(f'{vogal} = {texto.count(vogal)}')
         verificadas.append(vogal)
+    else:
+        continue
+    print(f'{vogal} = {texto.count(vogal)}')
